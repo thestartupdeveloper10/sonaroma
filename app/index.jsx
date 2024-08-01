c1ad289338632fc2e947c352d251368b2b8cf258
thestartupdeveloper10 <thestartupdeveloper0@gmail.com>
2024-08-01 11:33:44 +0300

    tab bar navigation

--- app/index.jsx
+++ app/index.jsx
@@ -1,11 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { Link } from "expo-router";
 
 export default function App() {
   return (
     <View className="bg-gray-900 flex-1 justify-center items-center">
-      <Text className="text-gray-400 text-3xl">sonoroma!</Text>
+      <Text className="text-gray-400 text-3xl font-pblack">sonoroma!</Text>
       <StatusBar style="auto" />
+      <Link href="/Home" className="text-gray-400 text-3xl font-pblack">Home</Link>
+      
     </View>
   );
 }
